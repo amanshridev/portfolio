439c21dd79513a3f15b6c52c050aeaea64fef1d9
amanshridev <mailtoamanshri@proton.me>
2024-05-05 13:39:48 +0530

    Breaking down code into components

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -4,38 +4,21 @@ import {
   AiFillLinkedin,
   AiFillYoutube,
 } from "react-icons/ai";
-import { BsFillMoonStarsFill } from "react-icons/bs";
 import Image from "next/image";
 import avatar from "../../public/avatar.png";
+import Navbar from '../NavBar';
+import { ReactTyped } from "react-typed";
 
 const About = () => {
   return (
     <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">Aman Shrivastav</h1>
-            <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill
-                  onClick={() => setDarkMode(!darkMode)}
-                  className=" cursor-pointer text-2xl"
-                />
-              </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <Navbar />
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Dimitri Marco
+              Aman Shrivastav
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+             I am a <ReactTyped className='font-semibold  text-teal-600' strings={["Front End Developer", "Web Designer", "Ui Designer"]} typeSpeed={80} backSpeed={50} loop />
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
               Freelancer providing services for programming and design content
